210f1747c196155af1f31e75ad3db7c4684d0ca0
Brondahl <stixmclean@googlemail.com>
2018-10-08 07:38:42 +0100

    Demo of Enum conversions

---
+++ MDMWebApiTemplate/Controllers/EnumSerialisationDemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MDMWebApiTemplate.Controllers
+{
+  /*
+   * Demonstrates that Enums are serialised to their enum names, rather than to the underlying int value.
+   */
+  public enum Choice
+  {
+    DefaultOption,
+    BestOption,
+    AlternativeOption
+  }
+
+  [Route("api/[controller]")]
+  [ApiController]
+  public class EnumSerialisationDemoController : ControllerBase
+  {
+    // GET api/values
+    [HttpGet("GetEnumValues")]
+    public ActionResult<Choice[]> Get()
+    {
+      return new [] { Choice.DefaultOption, Choice.BestOption, Choice.AlternativeOption };
+    }
+  }
+}
